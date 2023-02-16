c45ac79dea674659570aaa53f52ef028fc49438c
wangshan <wangshan_aqi@163.com>
2023-02-16 23:03:08 +0800

    feat: #自定义指令——pipe管道 新内容

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgswitchComponent } from './view/ngswitch/ngswitch.component';
-import { NgifComponent } from './view/ngif/ngif.component';
-import { NgbindComponent } from './view/ngbind/ngbind.component';
-import { NgclassComponent } from './view/ngclass/ngclass.component';
-import { NgmodelComponent } from './view/ngmodel/ngmodel.component';
 import { FormsModule } from '@angular/forms';
-import { NgforComponent } from './view/ngfor/ngfor.component';
 
 @NgModule({
   // 声明组件 管道符 指令
   declarations: [
     AppComponent,
-    NgswitchComponent,
-    NgifComponent,
-    NgbindComponent,
-    NgclassComponent,
-    NgmodelComponent,
-    NgforComponent
   ],
   // 模块 针对全局的
   imports: [
